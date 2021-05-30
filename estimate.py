8752ac535bbd07d54f5ab30c98ef7db42a3c8c4d
Shanley Thomas <shanley_b190901me.nitc.ac.in>
2021-05-30 13:36:04 +0530

    ESTIMATION OF PI

--- estimate.py
+++ estimate.py
@@ -1,32 +1,31 @@
-import math
-import unittest
-
-class TestWallis(unittest.TestCase):
-    def test_low_iters(self):
-        for i in range(0, 5):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
-    def test_high_iters(self):
-        for i in range(500, 600):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+import random
 
 
-class TestMC(unittest.TestCase):
-    def test_randomness(self):
-        pi0 = monte_carlo(15000)
-        pi1 = monte_carlo(15000)
-        
-        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
+def wallis(n):
+    t=n+1
+    pi=1
+    for i in range(1,t):
+         pi *= (((4*(i**2)))/((4*(i**2))-1))
+    pi *= 2
+    return pi
 
-        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
+def monte_carlo(n):
+    c = 0
+    tc = 0
+    for i in range(n):
+        x = random.random()
+        y = random.random()
+        distance = ((x**2)+(y**2))**(1/2)
+        if distance < 1:
+            c += 1
+            tc += 1
+        else:
+            tc += 1
+    pi=(4*(c/tc))
+    return pi
 
-    def test_accuracy(self):
-        for i in range(500, 600):
-            pi = monte_carlo(i)
-            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
-if __name__ == "__main__":
-    unittest.main()
+n=int(input("Enter the number of iterations: "))
+print(f"The estimate of PI using the wallis formula: {wallis(n)}")
+
+#a=int(input("enter the total number of iterations: "))
+print(f"The estimate of PI using the monte carlo simulation: {monte_carlo(n)}")
